156680ca4616af14a5154314d825fc22ca9d36a7
Ro DL <romideleon@gmail.com>
2024-12-12 17:15:37 -0300

    actualización search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,38 +3,54 @@
 ---
 // Based on a script by Kathie Decora : katydecorah.com/code/lunr-and-jekyll/
 
-//Create the lunr index for the search
 var index = elasticlunr(function () {
-  this.addField('section')
-  this.addField('pagenum')
+  this.addField('title')
   this.addField('layout')
+  //this.addField('date')
   this.addField('content')
   this.setRef('id')
 });
 
-{% assign count = 0 %}{% for text in site.hdlab.space %}
+
+{% assign count = 0 %}{% for post in site.posts %}
 index.addDoc({
-  section: {{text.section | jsonify}},
-  pagenum: {{text.pagenum | jsonify}},
-  layout: {{text.layout | jsonify}},
-  content: {{text.content | jsonify | strip_html}},
+  title: {{post.title | jsonify}},
+  layout: {{post.layout | jsonify}},
+  //date: {{post.date | jsonify}},
+  content: {{post.content | jsonify | strip_html}},
   id: {{count}}
 });{% assign count = count | plus: 1 %}{% endfor %}
 console.log( jQuery.type(index) );
 
-var store = [{% for text in site.hdlab.space %}{
-  "section": {{text.section | jsonify}},
-  "pagenum": {{text.pagenum | jsonify}},
-  "layout": {{ text.layout | jsonify }},
-  "link": {{text.url | jsonify}},
+{% for page in site.pages %}
+index.addDoc({
+  title: {{page.title | jsonify}},
+  layout: {{page.layout | jsonify}},
+  content: {{page.content | jsonify | strip_html}},
+  id: {{count}}
+});{% assign count = count | plus: 1 %}{% endfor %}
+console.log( jQuery.type(index) );
+
+var store1 = [{% for post in site.posts %}{
+  "title": {{post.title | jsonify}},
+  "layout": {{ post.layout | jsonify }},
+  "link": {{post.url | jsonify}},
 },
 {% endfor %}]
 
+var store2 = [{% for page in site.pages %}{ 
+  "title": {{page.title | jsonify}},
+  "layout": {{ page.layout | jsonify }},
+  "link": {{page.url | jsonify}},
+},
+{% endfor %}]
+
+let store = store1.concat(store2);
+
+//Query
 var url = window.location.href;
 if (url.lastIndexOf("?q=") > 0) {
-  // get the index of the parameter, add three (to account for length)
   var stringloc = url.lastIndexOf("?q=") + 3;
-  // get the substring (query) and decode
   var searchquery = decodeURIComponent(url.substr(stringloc));
 }
 
@@ -42,20 +58,19 @@ function doSearch() {
   var resultdiv = $('#results');
   var query = $('input#search').val();
 
-  //The search is then launched on the index built with Lunr
   var result = index.search(query);
   resultdiv.empty();
   if (result.length == 0) {
-    resultdiv.append('<p class="">No se encontraron resultados.</p>');
+    resultdiv.append('<p class="">No se encontraron resultados</p>');
   } else if (result.length == 1) {
     resultdiv.append('<p class="">'+result.length+' resultados</p>');
   } else {
     resultdiv.append('<p class="">'+result.length+' resultados</p>');
   }
-  //Loop through, match, and add results
+
   for (var item in result) {
     var ref = result[item].ref;
-    var searchitem = '<div class="result"><p><a href="{{ site.baseurl }}'+store[ref].link+'?q='+query+'">'+store[ref].section+' - '+store[ref].pagenum+'</a></p></div>';
+    var searchitem = '<div class="result"><p><a href="{{ site.baseurl }}'+store[ref].link+'?q='+query+'">'+store[ref].title+'</a></p></div>';
     resultdiv.append(searchitem);
   }
 }
